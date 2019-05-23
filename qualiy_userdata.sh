88e5989762655d9e76750788f92dcddfb46e37ee
Gil <GilGald@users.noreply.github.com>
2019-05-23 11:22:35 +0300

    Update qualiy_userdata.sh

--- qualiy_userdata.sh
+++ qualiy_userdata.sh
@@ -28,7 +28,7 @@ echo 'Staring guacamole-client...'
 if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
 	docker run -d --name guacamole-client --link guacamole-server:guacd -p 8080:8080 quali/guacamole-client:0.9.14
 else 
-	docker run -d --name guacamole-client --link guacamole-server:guacd -p 8080:8080 -p 8000:8000 -e $1 -e $2 -e $3 dockergil90/qualiy_debug
+	docker run -d --name guacamole-client --link guacamole-server:guacd -p 8080:8080 -p 8000:8000 -e $1 -e $2 -e $3 dockergil90/qualiy_debug:1.0.1
 fi
 
 echo 'End of QualiY user-data'
